67bc7e17ed7cd7a26f548c6187420a2cb3f737f5
Vishal-Bhat-1 <91203554+Vishal-Bhat-1@users.noreply.github.com>
2023-09-22 14:31:25 -0400

    Initial commit

---
+++ src/App/App.style.js
@@ -0,0 +1,14 @@
+import * as colors from "./standard-colors";
+
+export const buttonStyle = {
+  padding: "0.5vh",
+  fontSize: "1.5vh",
+  backgroundColor: colors.pink,
+  color: "#FFF",
+  fontWeight: "bold",
+  "&:hover": {
+    backgroundColor: colors.pink,
+    color: "#FFF",
+    fontWeight: "bold",
+  },
+};
